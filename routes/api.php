17196bf71a4ec6e0564627175c5c5408fede515f
Miriam <miriamsd94@gmail.com>
2026-08-10 17:47:31 +0200

    feat: implement user profile endpoints with authorization checks added routes and UserController

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 
 // Rutas públicas
 Route::post('/register', [AuthController::class, 'register']);
@@ -11,4 +12,10 @@ Route::post('/login', [AuthController::class, 'login']);
 // Rutas protegidas (Requieren enviar el Token)
 Route::middleware('auth:api')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+
+    // Rutas de Estudiantes (Users)
+    Route::get('/users/{user}', [UserController::class, 'show']);
+    Route::put('/users/{user}', [UserController::class, 'update']);
+    Route::delete('/users/{user}', [UserController::class, 'destroy']);
+
 });
