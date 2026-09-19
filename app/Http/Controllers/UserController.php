284b39180bae4482ec38d6df4ae69f4469710b8c
Miriam <miriamsd94@gmail.com>
2026-09-19 17:55:58 +0200

    feat: implement cv file upload and storage cleanup and add secure cv file serving logic

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,6 +8,8 @@ use Illuminate\Http\JsonResponse;
 use App\Http\Requests\UpdateUserRequest;
 use App\Http\Resources\UserResource;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Storage;
+
 
 class UserController extends Controller
 {
@@ -50,8 +52,22 @@ class UserController extends Controller
 
         //Uso de la policy (verifica si puede hacer update, si da false devuelve un 403)
         Gate::authorize('update', $user);
-            
-        $user->update($request->validated());
+
+        // Obtenemos TODOS los datos validados del request
+        $data = $request->validated();
+
+        if ($request->hasFile('cv')) {
+            // Borramos el anterior si existía
+            if ($user->cv_path) {
+                Storage::delete($user->cv_path);
+            }
+        
+            // Guardamos el archivo en la carpeta 'cvs' (dentro de storage/app)
+            $path = $request->file('cv')->store('cvs');
+            $data['cv_path'] = $path; // Esto es lo que se guarda en la BD
+        }
+        // Actualizamos y devolvemos el Resource
+        $user->update($data);
 
         return new UserResource($user);
     }
